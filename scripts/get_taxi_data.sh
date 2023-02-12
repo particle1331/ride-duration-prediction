9a672b0570c30b69413347527f69514d93a9dad5
particle <54845098+particle1331@users.noreply.github.com>
2023-02-13 02:58:27 +0800

    add initial files

---
+++ scripts/get_taxi_data.sh
@@ -0,0 +1,2 @@
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2021-01.parquet -O ../data/green_tripdata_2021-01.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2021-02.parquet -O ../data/green_tripdata_2021-02.parquet
