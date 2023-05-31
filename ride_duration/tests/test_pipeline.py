fe8e6ba05100cb9c4e0f820124e8b6b7116e88c6
particle1331 <54845098+particle1331@users.noreply.github.com>
2023-06-01 05:50:45 +0800

    Simplify tests with fixtures

--- ride_duration/tests/test_pipeline.py
+++ ride_duration/tests/test_pipeline.py
@@ -1,11 +1,10 @@
 import os
 import math
-import pytest
 
 import numpy as np
 import joblib
 import pandas as pd
-
+import pytest
 from sklearn.metrics import mean_squared_error
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
@@ -17,18 +16,13 @@ from ride_duration.config import MODEL_DIR, DATASET_DIR, config
 from ride_duration.processing import prepare_features
 
 
-def test_pipeline_training():
+def test_pipeline_training(train, valid):
     """Run training pipeline."""
 
-    # Load raw dataset
-    train = pd.read_parquet(DATASET_DIR / config.TRAIN_SAMPLE)
-    valid = pd.read_parquet(DATASET_DIR / config.VALID_SAMPLE)
-
-    # Note: Feature selection defaults to config.FEATURES
     X_train, y_train = prepare_features(train, train=True)
     X_valid, y_valid = prepare_features(valid, train=True)
 
-    # Fit model pipeline (all trainable here)
+    # Fit model pipeline (transforms (trainable) + model)
     pipe = make_pipeline(
         FunctionTransformer(convert_to_dict), DictVectorizer(), LinearRegression()
     )
@@ -36,28 +30,20 @@ def test_pipeline_training():
     pipe.fit(X_train, y_train)
 
     # Check performance
-    p_train = pipe.predict(X_train)
-    p_valid = pipe.predict(X_valid)
+    pred_train = pipe.predict(X_train)
+    pred_valid = pipe.predict(X_valid)
 
-    mse_train = mean_squared_error(y_train, p_train, squared=False)
-    mse_valid = mean_squared_error(y_valid, p_valid, squared=False)
+    mse_train = mean_squared_error(y_train, pred_train, squared=False)
+    mse_valid = mean_squared_error(y_valid, pred_valid, squared=False)
 
-    # Persist trained model
-    model_path = MODEL_DIR / "model_pipe.pkl"
-    joblib.dump(pipe, model_path)
-
-    assert os.path.exists(model_path)
     assert mse_train <= 12.0
     assert mse_valid <= 15.0
 
 
-def test_pipeline_inference():
+def test_pipeline_inference(model, valid):
     """Running inference pipeline. Same transforms as above test."""
 
-    data = pd.read_parquet(DATASET_DIR / config.VALID_SAMPLE)
-    model = joblib.load(MODEL_DIR / config.MODEL_SAMPLE)
-
-    X = prepare_features(data)[0]
+    X = prepare_features(valid)[0]
     pred = model.predict(X)
 
     assert math.isclose(pred.mean(), 16.0, abs_tol=5.0)
--- ride_duration/tests/test_processing.py
+++ ride_duration/tests/test_processing.py
@@ -5,10 +5,9 @@ from ride_duration.config import DATASET_DIR, config
 from ride_duration.processing import preprocess, prepare_features
 
 
-def test_preprocess_train():
-    df = pd.read_parquet(DATASET_DIR / config.TRAIN_SAMPLE)
-    orig_shape = df.shape
-    df = preprocess(df, train=True)
+def test_preprocess_train(train):
+    orig_shape = train.shape
+    df = preprocess(train, train=True)
 
     assert sorted(list(df.columns)) == sorted(config.FEATURES + [config.TARGET])
     assert df.shape[0] <= orig_shape[0]
@@ -16,10 +15,9 @@ def test_preprocess_train():
     assert config.TARGET in df.columns
 
 
-def test_preprocess_infer():
-    df = pd.read_parquet(DATASET_DIR / config.TRAIN_SAMPLE)
-    orig_shape = df.shape
-    df = preprocess(df, train=False)
+def test_preprocess_infer(train):
+    orig_shape = train.shape
+    df = preprocess(train, train=False)
 
     assert sorted(list(df.columns)) == sorted(config.FEATURES)
     assert df.shape[0] == orig_shape[0]
@@ -27,27 +25,26 @@ def test_preprocess_infer():
     assert config.TARGET not in df.columns
 
 
-def test_prepare_features():
-    df = pd.read_parquet(DATASET_DIR / config.TRAIN_SAMPLE)
-    X_train, y_train = prepare_features(df, train=True)
-    X_infer, y_infer = prepare_features(df)
+def test_prepare_features(train):
+    X_train, y_train = prepare_features(train, train=True)
+    X_infer, y_infer = prepare_features(train)
 
     assert y_infer is None
     assert y_train is not None
     assert X_train.shape[1] == len(config.NUM_FEATURES + config.CAT_FEATURES)
     assert X_infer.shape[1] == len(config.NUM_FEATURES + config.CAT_FEATURES)
-    assert X_infer.shape[0] == df.shape[0]
-    assert X_train.shape[0] <= X_infer.shape[0]  # Filtered vs. Not filtered
+    assert X_infer.shape[0] == train.shape[0]
+    assert X_train.shape[0] <= X_infer.shape[0]  # filtered vs. not filtered
 
 
-def test_transforms():
-    df = pd.read_parquet(DATASET_DIR / config.TRAIN_SAMPLE)
-
-    # Note that order matters:
+def test_transforms(train):
     select_features = config.NUM_FEATURES[:1] + config.CAT_FEATURES[:1]
-    transforms = [lambda x: x[select_features], lambda x: convert_to_dict(x)]
-    X, _ = prepare_features(df, transforms=transforms)
+    transforms = [
+        lambda x: x[select_features],
+        lambda x: convert_to_dict(x),
+    ]  # order matters
+    X, _ = prepare_features(train, transforms=transforms)
 
-    assert len(X) == len(df)
+    assert len(X) == len(train)
     assert isinstance(X[0], dict)
     assert sorted(X[0].keys()) == sorted(select_features)
